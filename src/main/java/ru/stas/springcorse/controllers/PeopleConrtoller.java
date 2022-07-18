67e486b4c8f3060f155c2fbdd68aece9bb09a7bd
frontend12 <stasstykov@gmail.com>
2022-07-18 12:43:12 +0300

    peopleDAO

--- src/main/java/ru/stas/springcorse/controllers/PeopleConrtoller.java
+++ src/main/java/ru/stas/springcorse/controllers/PeopleConrtoller.java
@@ -1,26 +1,34 @@
 package ru.stas.springcorse.controllers;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.stas.springcorse.controllers.dao.PersonDAO;
 
 @Controller
 @RequestMapping("/people")
 public class PeopleConrtoller {
 
+    private final PersonDAO personDAO;
+
+    public PeopleConrtoller(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
     @GetMapping()
     public String index(Model model){
-        //Получим всех людей из DAO и передадим отображение в представление
-        return null;
+        model.addAttribute("people",personDAO.index());
+        return "people/index";
     }
 
     @GetMapping("/{id}")
     public  String show(@PathVariable("id") int id, Model model){
-        //Получим одного человека  по id из  DAO и передадим на отображение в представлении
-        return null;
+        model.addAttribute("person",personDAO.show(id));
+        return "people/show";
     }
 }
--- src/main/java/ru/stas/springcorse/controllers/dao/PersonDAO.java
+++ src/main/java/ru/stas/springcorse/controllers/dao/PersonDAO.java
@@ -1,10 +1,12 @@
 package ru.stas.springcorse.controllers.dao;
 
+import org.springframework.stereotype.Component;
 import ru.stas.springcorse.controllers.models.Person;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class PersonDAO {
     private static int PEOPLE_COUNT;
     private List<Person> people;
@@ -17,5 +19,14 @@ public class PersonDAO {
         people.add(new Person(++PEOPLE_COUNT, "Coc"));
         people.add(new Person(++PEOPLE_COUNT, "Maik"));
         people.add(new Person(++PEOPLE_COUNT,"Karl"));
+    }
+
+    public List<Person> index(){
+        return people;
+    }
+
+    public Person show(int id){
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+
     }
 }
