6d35a72afdc3cb50ea8a891869d71c9be59872ab
frontend12 <stasstykov@gmail.com>
2022-07-20 17:34:55 +0300

    peopleDAO

--- src/main/java/ru/stas/springcorse/controllers/PeopleConrtoller.java
+++ src/main/java/ru/stas/springcorse/controllers/PeopleConrtoller.java
@@ -5,10 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.stas.springcorse.controllers.dao.PersonDAO;
+import ru.stas.springcorse.controllers.models.Person;
 
 @Controller
 @RequestMapping("/people")
@@ -31,5 +30,19 @@ public class PeopleConrtoller {
     public  String show(@PathVariable("id") int id, Model model){
         model.addAttribute("person",personDAO.show(id));
         return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(Model model){
+        model.addAttribute("person", new Person());
+        return "people/new";
+
+    }
+
+    @PostMapping
+    public String create(@ModelAttribute("person") Person person){
+        personDAO.save(person);
+        return "redirect:/people";
+
     }
 }
--- src/main/java/ru/stas/springcorse/controllers/models/Person.java
+++ src/main/java/ru/stas/springcorse/controllers/models/Person.java
@@ -4,10 +4,14 @@ public class Person {
 
     private int id;
     private String name;
+    public Person(){
+
+    }
 
     public Person(int id, String name) {
         this.id = id;
         this.name = name;
+
     }
 
     public int getId() {
